bab781441955578ec75336537e24039e8c731967
Electronic-Waste <2690692950@qq.com>
2023-12-01 10:37:35 +0800

    fix: update container image.

--- scripts/env_v2_qimeng.sh
+++ scripts/env_v2_qimeng.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-~/dtn-satellite-sdn/bin/sdnctl init -u http://localhost:32121/Location/Location -n 7 -v v2 -i 300 --test
+nohup ~/dtn-satellite-sdn/bin/sdnctl init -u http://localhost:32121/Location/Location \
+    -n 7 -v v2 -i 300 --test > ../log/qimeng.log 2>&1 &
